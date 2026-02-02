869b43753b7438b41311d7aab40d5a342dc6d635
mathyx3 <matheussaraiva6969@gmail.com>
2026-02-02 16:09:53 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// Menu Hambúrguer
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('open');
+});
+
+// Transições suaves para links do menu
+document.querySelectorAll('#nav-menu a').forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+        navMenu.classList.remove('open'); // Fecha o menu após clicar
+    });
+});
